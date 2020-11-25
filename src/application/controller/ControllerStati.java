e03a478b606ffb0a2cff8e6f9cf5e4a918451ef6
david <dstraeus@htl-steyr.ac.at>
2020-11-25 10:18:54 +0100

    Stati Auftrag Update

--- src/application/controller/ControllerStati.java
+++ src/application/controller/ControllerStati.java
@@ -1,21 +1,29 @@
 package application.controller;
 
 import application.model.Status;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 
 public class ControllerStati {
     public TextField nameField;
+    public Button abbrechenButton;
+    private int num = 0;
+
     public ListView<Status> statiList;
     ObservableList<Status> liste = FXCollections.observableArrayList();
+
+    private Status selectedItem = null;
 
     public void initialize(){loadFile();}
 
@@ -36,6 +44,7 @@ public class ControllerStati {
                         s.status = split[1];
 
                         liste.add(s);
+                        num++;
 
                     }
                 } finally {
@@ -49,12 +58,70 @@ public class ControllerStati {
         }
 
 
-    public void speichernClicked(ActionEvent actionEvent) {
-    }
+    public void statiClicked(MouseEvent mouseEvent) {
+        nameField.setText(statiList.getSelectionModel().getSelectedItem().status);
 
-    public void löschenClicked(ActionEvent actionEvent) {
+        selectedItem = statiList.getSelectionModel().getSelectedItem();
     }
 
     public void neuClicked(ActionEvent actionEvent) {
+        nameField.clear();
+
+        selectedItem = null;
+    }
+
+    public void speichernClicked(ActionEvent actionEvent) {
+        if(selectedItem != null){
+            selectedItem.status = nameField.getText();
+
+            statiList.refresh();
+
+            statiList.setItems(liste);
+        } else {
+            if(!nameField.getText().isEmpty()){
+                Status s = new Status();
+
+                s.status = nameField.getText();
+                s.nummer = num + 1;
+
+                liste.add(s);
+            }
+            num++;
+        }
+        writeFile();
+    }
+
+    public void löschenClicked(ActionEvent actionEvent) {
+        nameField.clear();
+        num--;
+        liste.remove(selectedItem);
+        writeFile();
+    }
+
+
+    private void writeFile() {
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("stati.csv"));
+            try {
+
+                for (Status s : liste) {
+                    bw.write(s.nummer + ";" + s.status + "\n");
+                }
+
+                bw.flush();
+            } finally {
+                bw.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void abbrechenClicked(ActionEvent actionEvent) {
+        Stage stage = (Stage) abbrechenButton.getScene().getWindow();
+        stage.close();
     }
 }
