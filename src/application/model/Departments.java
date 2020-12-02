1d17d7bd9963cb1bf1d0abd43b949aeb14d27749
david <dstraeus@htl-steyr.ac.at>
2020-12-02 10:54:48 +0100

    Departments fertig

---
+++ src/application/model/Departments.java
@@ -0,0 +1,11 @@
+package application.model;
+
+public class Departments {
+
+    public int nummer = 0;
+    public String department = "";
+
+    public String toString() {
+        return nummer + "-" + department;
+    }
+}
