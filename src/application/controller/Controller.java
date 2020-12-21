d51c9dd8986e7a3dd407e8e48e5201bdc0928d8a
cookiieez <ereichel@htl-steyr.ac.at>
2020-12-21 15:04:11 +0100

    priority list

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -8,7 +8,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -107,19 +106,6 @@ public class Controller {
         ticketListView.setItems(searchStatiList);
     }
 
-    public void prioSearch(ActionEvent actionEvent) {
-        int searched = Integer.parseInt(filterPriorityComboBox.getSelectionModel().getSelectedItem().priority);
-
-        searchPrioList.clear();
-
-        for (Ticket t : liste) {
-            if (t.id == searched) {
-                searchPrioList.add(t);
-            }
-        }
-
-        ticketListView.setItems(searchPrioList);
-    }
 
     public void saveClicked(ActionEvent actionEvent) {
         //Wenn Ticket neu -> laden des Tickets und hinzufügen zur Liste
@@ -168,4 +154,16 @@ public class Controller {
     }
 
 
+    public void prioSearch(ActionEvent actionEvent) {
+        String searched = filterPriorityComboBox.getSelectionModel().getSelectedItem().priority;
+        searchPrioList.clear();
+
+        for (Ticket t : liste) {
+            if (t.id == Integer.parseInt(searched)) {
+                searchPrioList.add(t);
+            }
+        }
+
+        ticketListView.setItems(searchPrioList);
+    }
 }
